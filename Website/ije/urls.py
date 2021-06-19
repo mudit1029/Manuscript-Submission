69ca5a1082daa5c328e293dacc4745c3371bd148
Mudit <muditagarwal1029@gmail.com>
2021-06-19 19:20:08 +0530

    Added data in two pages

--- Website/ije/urls.py
+++ Website/ije/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "ije"
+
 urlpatterns = [
 	path('', views.index , name="index"),
+	path('journal-info/peer-review-process/', views.prp, name="prp"),
+	path('journal-info/about-journal/', views.aj, name="aj"),
+	path('journal-info/publication-ethics/', views.pe, name="pe"),
 ]
--- Website/ije/views.py
+++ Website/ije/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return render(request, "ije/index.html")
+
+def prp(request):
+	data = "Once a paper is submitted online, editor in the related field as an expert should check the paper. After primary screening the paper send to reviewers. The reviewer respond has to be presented to Editorial board meeting for further evaluation. From the review process and editorial board evaluation we decide for revision or reject. In the case of a paper Editorial board is unable to judge the given comments by the reviewer and the revised paper. As author is asked for revision, the whole folder send to final referee for the exact judgment. Author at revision stage must prepare rebuttal letter for respond to itemized comments given by reviewer. Editorial board based on complete cycle of revision if the revision is satisfied may vote for acceptance."	
+	return render(request, "ije/index.html" ,{
+		"prp": data
+		})
+
+def aj(request):
+	pass
+
+def pe(request):
+	return render(request, "ije/pe.html")	
